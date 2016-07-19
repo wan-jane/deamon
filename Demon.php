9ef893ce0c30bcad03f254ffc15b092f0eb8df69
wanphpChina <18618300482@163.com>
2016-07-19 16:33:46 +0800

    Update Demon.php

--- Demon.php
+++ Demon.php
@@ -1,90 +1,102 @@
 <?php
+
+/**
+ * User: gengkang
+ * Date: 16-7-19
+ * Time: 下午3:41
+ */
 class Demon {
-	/* config */
-	const HOST = '192.168.2.1';
-	const PORT = 6379;
-	const MAXCONN = 2048;
+    /* config */
+    const HOST = '127.0.0.1';
+    const PORT = 6379;
+    const MAXCONN = 2048;
+    const pidfile = __CLASS__;
+    const uid = 80;
+    const gid = 80;
 
-	const pidfile = __CLASS__;
-	const uid	= 80;
-	const gid	= 80;
-	
-	protected $pool = NULL;
-	protected $redis = NULL;
+    protected $pool = NULL;
+    protected $redis = NULL;
 
-	public function __construct() {
-		$this->pidfile = '/var/run/'.self::pidfile.'.pid';
-		$this->redis = new Redis();
-	}
-	private function daemon(){
-		if (file_exists($this->pidfile)) {
-			echo "The file $this->pidfile exists.\n";
-			exit();
-		}
-		
-		$pid = pcntl_fork();
-		if ($pid == -1) {
-			 die('could not fork');
-		} else if ($pid) {
-			 // we are the parent
-			 //pcntl_wait($status); //Protect against Zombie children
-			exit($pid);
-		} else {
-			// we are the child
-			file_put_contents($this->pidfile, getmypid());
-			posix_setuid(self::uid);
-			posix_setgid(self::gid);
-			return(getmypid());
-		}
-	}
-	private function run(){
-		$this->pool = new Pool(self::MAXCONN, \ExampleWorker::class, []);
+    public function __construct() {
+        $this->pidfile = __DIR__ . '/' . self::pidfile . '.pid';
+        $this->redis = new Redis();
+    }
 
-		$this->redis->connect(self::HOST, self::PORT);
-		$channel = array('news', 'login', 'logout');
-		$this->redis->subscribe($channel, 'callback');
-		function callback($instance, $channelName, $message) {
-  			echo $channelName, "==>", $message,PHP_EOL;
-			//print_r($message);
-			$this->pool->submit(new Fee($message));
-		}
-	
-		$pool->shutdown();	
-	}
-	private function start(){
-		$pid = $this->daemon();
-		$this->run();
-	}
-	private function onestart(){
-		$this->run();
-	}	
+    private function daemon() {
+        if (file_exists($this->pidfile)) {
+            echo "The file $this->pidfile exists.\n";
+            exit();
+        }
 
-	private function stop(){
+        $pid = pcntl_fork();
+        if ($pid == -1) {
+            die('could not fork');
+        } else if ($pid) {
+            // we are the parent
+            //pcntl_wait($status); //Protect against Zombie children
+            exit($pid);
+        } else {
+            // we are the child
+            file_put_contents($this->pidfile, getmypid());
+            posix_setuid(self::uid);
+            posix_setgid(self::gid);
+            return (getmypid());
+        }
+    }
 
-		if (file_exists($this->pidfile)) {
-			$pid = file_get_contents($this->pidfile);
-			posix_kill($pid, 9); 
-			unlink($this->pidfile);
-		}
-	}
-	private function help($proc){
-		printf("%s start | stop | onestart | help \n", $proc);
-	}
-	public function main($argv){
-		if(count($argv) < 2){
-			printf("please input help parameter\n");
-			exit();
-		}
-		if($argv[1] === 'stop'){
-			$this->stop();
-		} else if($argv[1] === 'start'){
-			$this->start();
-		} else if ($argv[1] === 'onestart') {
-			$this->onestart();
-		} else{
-			$this->help($argv[0]);
-		}
-	}
+    private function run() {
+        try {
+            $this->redis->connect(self::HOST, self::PORT);
+            $channel = array('news', 'login', 'logout');
+            $this->redis->subscribe($channel, [$this, 'handle']);
+        } catch (Exception $e) {
+            print_r("异常：" . $e->getMessage());
+            print_r("\n 重新链接频道.....\n");
+            $this->run();
+            print_r("\n 重新链接成功.....\n");
+        }
+    }
+
+    private function start() {
+        $pid = $this->daemon();
+        $this->run();
+    }
+
+    private function onestart() {
+        $this->run();
+    }
+
+    private function stop() {
+        if (file_exists($this->pidfile)) {
+            $pid = file_get_contents($this->pidfile);
+            posix_kill($pid, 9);
+            unlink($this->pidfile);
+        }
+    }
+
+    private function help($proc) {
+        printf("%s start | stop | help \n", $proc);
+    }
+
+    public function main($argv) {
+        if (count($argv) < 2) {
+            printf("please input help parameter\n");
+            exit();
+        }
+        if ($argv[1] === 'stop') {
+            $this->stop();
+        } else if ($argv[1] === 'start') {
+            $this->start();
+        //} else if ($argv[1] === 'onestart') {
+            //$this->onestart();
+        } else {
+            $this->help($argv[0]);
+        }
+    }
+
+    public function handle($instance, $channelName, $message) {
+        file_put_contents(__DIR__ . "/$channelName.txt", $message . "\n", FILE_APPEND);
+    }
 }
 
 $example = new Demon();
